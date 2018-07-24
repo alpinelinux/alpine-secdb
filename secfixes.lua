dd31d1534d2aa0516d0751e9715b72cc9c732be3
Natanael Copa <ncopa@alpinelinux.org>
2018-07-24 14:55:47 +0200

    add v3.8

--- secfixes.lua
+++ secfixes.lua
@@ -74,6 +74,7 @@ archs = {
 	["v3.5"] = { "x86_64", "x86", "armhf", "aarch64" },
 	["v3.6"] = { "x86_64", "x86", "armhf", "aarch64", "ppc64le", "s390x" },
 	["v3.7"] = { "x86_64", "x86", "armhf", "aarch64", "ppc64le", "s390x" },
+	["v3.8"] = { "x86_64", "x86", "armhf", "aarch64", "ppc64le", "s390x" },
 }
 
 opts, args = require('optarg').from_opthelp(opthelp)
